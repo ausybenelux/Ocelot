0f9440242c59ef80e6895c760242f2a6835c90de
Robbie Bardijn <robbie.bardijn@gmail.be>
2014-02-14 08:46:45 +0100

    cleaned out css, ignored right things, renamed gemfile.rb to Gemfile and added template and templates

---
+++ templates/html.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php print $rdf_namespaces; ?> class="no-js"
+  lang="<?php print $language->language; ?>">
+  <head>
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+  </head>
+  <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
+  </body>
+</html>
