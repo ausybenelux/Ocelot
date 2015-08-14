b3fdfbd2ac8582b704e7e41fd4e540818d450687
Sander Tirez <sander.tirez@Panda.local>
2015-08-14 14:25:08 +0200

    Issue #44: Add gulp sass task, including bourbon, bourbon neat and font-awesome support.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,70 @@
+//    ______   __    __  __        _______
+//   /      \ /  |  /  |/  |      /       \
+//  /$$$$$$  |$$ |  $$ |$$ |      $$$$$$$  |
+//  $$ | _$$/ $$ |  $$ |$$ |      $$ |__$$ |
+//  $$ |/    |$$ |  $$ |$$ |      $$    $$/
+//  $$ |$$$$ |$$ |  $$ |$$ |      $$$$$$$/
+//  $$ \__$$ |$$ \__$$ |$$ |_____ $$ |
+//  $$    $$/ $$    $$/ $$       |$$ |
+//   $$$$$$/   $$$$$$/  $$$$$$$$/ $$/
+//
+
+// Gulp and some tools
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var chalk = require('chalk');
 var tasks = require('gulp-task-listing');
+
+// Sass
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var mincss = require('gulp-minify-css');
+
+// Load Ocelot configuration file
+var config = require('./ocelot.config.json');
+
+// -----------------------------------------------------------------------------
+// SASS
+// -----------------------------------------------------------------------------
+
+gulp.task("sass", function () {
+  return gulp.src(config.path.scss)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: [
+        require("node-bourbon").includePaths,
+        require("node-neat").includePaths[1],
+        require("node-normalize-scss").includePaths,
+        config.path.bower + config.path.fontAwesome
+      ],
+      outputStyle: config.sass.style
+    }))
+    .on("error", function (err) {
+      gutil.log(gutil.colors.black.bgRed(" SASS ERROR", gutil.colors.red.bgBlack(" " + (err.message.split("  ")[2]))));
+      gutil.log(gutil.colors.black.bgRed(" FILE:", gutil.colors.red.bgBlack(" " + (err.message.split("\n")[0]))));
+      gutil.log(gutil.colors.black.bgRed(" LINE:", gutil.colors.red.bgBlack(" " + err.line)));
+      gutil.log(gutil.colors.black.bgRed(" COLUMN:", gutil.colors.red.bgBlack(" " + err.column)));
+      return this.emit("end");
+    })
+    .pipe(prefix(config.autoprefixer))
+    .pipe(mincss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.path.css));
+});
+
+// -----------------------------------------------------------------------------
+// WATCH
+// -----------------------------------------------------------------------------
+
+gulp.task('watch', function() {
+  gulp.watch(config.path.scss, ['sass']);
+});
+
+// -----------------------------------------------------------------------------
+// DEFAULT TASK
+// -----------------------------------------------------------------------------
 
 gulp.task('help', tasks);
 gulp.task('default', ['help']);
