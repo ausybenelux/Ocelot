3e748eaa45884536ec9a9cd58ac910d2f3f05bc9
Dries Decramer <dries.decramer@one-agency.be>
2015-09-25 11:43:23 +0200

    issue #125: Added settings object - timings, breakpoints & debounce

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,5 +1,23 @@
 (function($, Drupal) {
-  var helper, site;
+  var helper, settings, site;
+
+  settings = {
+    timings: {
+      speedy: 200,
+      fast: 400,
+      normal: 600,
+      slow: 800
+    },
+      breakpoints: {
+      mobile-landscape: 480,
+      tablet-portrait: 768,
+      desktop: 960,
+      widescreen: 1200
+    },
+    debounce: {
+      medium: 200
+    }
+  };
 
   helper = {
     scrollTo: function(target) {
@@ -7,7 +25,7 @@
 
       if ($(target).offset() !== null) {
         topoffset = 70;
-        speed = 600;
+        speed = settings.timings.normal;
         destination = $(target).offset().top - topoffset;
         $("html:not(:animated),body:not(:animated)").animate({
           scrollTop: destination
